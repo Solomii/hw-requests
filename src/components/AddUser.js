dbdc0d73800be6ed5d8ff0cefde8d0e72e33730a
Solomii <kushniryksolomiya@gmail.com>
2022-05-04 16:48:51 +0300

    [+] Add AddUser component

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const AddUser = () => {
+const AddUser = ({onAdd}) => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    onAdd(e.target.name.value, e.target.email.value);
+    e.target.name.value = "";
+    e.target.email.value = "";
+
+  }
   return (
-    <div>AddUser</div>
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <p>Add user</p>
+        <input placeholder='name...' name="name" />
+        <input placeholder='email...' name="email" />
+        <button onSubmit={handleOnSubmit}>Add</button>
+      </form>
+
+    </div>
   )
 }
 
-export default AddUser
+export default AddUser;
